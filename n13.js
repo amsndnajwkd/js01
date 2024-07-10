c4015d80351e2970ff3a1bc3ad8a501e98ae444b
amsndnajwkd <studynsy@naver.com>
2024-07-10 15:07:47 +0900

    파일저장

--- n13.js
+++ n13.js
@@ -22,15 +22,11 @@ app.get("/", (req, res) => {
 
 app.get("/story", (req, res) => {
   const arr = [
-    "My life is pretty",
-    "Egg is Life",
-    "Cute & I do not have cat",
-    "Avengers are Dead",
-    "kkkkkkkkkkkkkkkkk",
+
   ];
-  const title = ["Pretty", "Egg", "Dog", "Avengers", "kkkk"];
+  const data = ["Pretty", "Egg", "Dog", "Avengers", "kkkk"];
   let list = `<h1>링크를 선택하세요</h1><h2><ul>`;
-  title.forEach(
+  data.forEach(
     (v, i) => (list += `<li><a href="/story?id=${i}">${v}</a></li>`)
   );
   list += `</ul></h2> ${arr[req.query.id] ?? "선택하세요"}`;
--- n14.js
+++ n14.js
@@ -5,16 +5,18 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send('안녕하세요! Express로 만든 서버입니다.')
+  res.send("<h1>안녕하세요! Express로 만든 서버입니다.</h1><h2><a href='/list'>리스트이동</a></h2>");
 });
-
-app.get('/list', (req,res) => {
-    fs.readdir(__dirname, 'utf-8', (err,data) => {
-        res.send(data);
-    });
+app.use("/list", express.static(__dirname));
+app.get("/list", (req, res) => {
+  fs.readdir(__dirname, "utf-8", (err, data) => {
+    let list = `<h1>링크를 선택하세요</h1><h2><ul>`;
+    data.forEach((v) => (list += `<li><a href="${v}">${v}</a></li>`));
+    list += `</ul></h2>`;
+    res.send(list);
+  });
 });
-
 
 app.listen(port, () => {
-      console.log(port + "에서 서버가 시작되었습니다.");
-    });
+  console.log(port + "에서 서버가 시작되었습니다.");
+});
